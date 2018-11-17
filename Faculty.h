b7321f9d7b1a83dcb165c743506fc4c566ad3cd1
MatthewParnham <parnham@chapman.edu>
2018-11-16 20:50:42 -0800

    faculty almost done

--- Faculty.h
+++ Faculty.h
@@ -1,6 +1,24 @@
 #ifndef FACULTY_H
 #define FACULTY_H
 
+#include <iostream>
+#include "GenDoublyLL.h"
+
+using namespace std;
+
+class Faculty : public Person {
+public:
+  Faculty();
+  ~Faculty();
+
+  string getDepartment();
+  void setDepartment(string d);
+
+  GenDoublyLL adviseeList;
+private:
+  string department;
+
+};
 
 
 #endif
