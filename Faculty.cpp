acd536d103ce42baf702cf503962d65120b1dee6
MatthewParnham <parnham@chapman.edu>
2018-11-16 20:55:19 -0800

    Faculty complete.

---
+++ Faculty.cpp
@@ -0,0 +1,18 @@
+#include "Faculty.h"
+#include <iostream>
+
+using namespace std;
+
+Faculty() {
+
+}
+~Faculty() {
+
+}
+
+string getDepartment() {
+  return department;
+}
+void setDepartment(string d) {
+  department = d;
+}
