aa983effb8a1884c9085cea2a3f53f979df9275a
MatthewParnham <parnham@chapman.edu>
2018-11-16 21:18:30 -0800

    set up main.

--- Main.cpp
+++ Main.cpp
@@ -6,3 +6,17 @@
 #include "Person.h"
 #include "Student.h"
 #include "GenTreeNode.h"
+#include "GenDoublyLL.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  // Check if save file exists
+
+  // Load save files
+
+  // Menu loop
+
+  // Save 
+  return 0;
+}
